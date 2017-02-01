b5a61ee8c89a67f0a67392c4dc7894ac3c142ff2
Sych Yuriy <sichyuriyy@gmail.com>
2017-02-02 01:36:40 +0200

    BigInteger karatsuba operation added

--- Homework1/src/main/java/com/gmail/at/sichyuriyy/javatraining/homework1/KaratsubaMultiplier.java
+++ Homework1/src/main/java/com/gmail/at/sichyuriyy/javatraining/homework1/KaratsubaMultiplier.java
@@ -1,9 +1,38 @@
 package com.gmail.at.sichyuriyy.javatraining.homework1;
 
+import java.math.BigInteger;
+
 /**
  * Created by Yuriy on 31.01.2017.
  */
 public class KaratsubaMultiplier {
+
+    public BigInteger karaMultiply(BigInteger x, BigInteger y) {
+        if (x.equals(BigInteger.ZERO)
+                || y.equals(BigInteger.ZERO)) {
+            return BigInteger.ZERO;
+        }
+
+        int shiftLength = Math.max(x.bitLength(), y.bitLength()) / 2;
+
+        if (shiftLength == 0) {
+            return x.multiply(y);
+        }
+
+        BigInteger a = x.shiftRight(shiftLength);
+        BigInteger b = x.subtract(a.shiftRight(shiftLength));
+        BigInteger c = y.shiftRight(shiftLength);
+        BigInteger d = y.subtract(c.shiftLeft(shiftLength));
+
+        BigInteger productAC = karaMultiply(a, c);
+        BigInteger productBD = karaMultiply(b, d);
+        BigInteger productABCD = karaMultiply(a.add(b), c.add(d));
+
+        return productAC.shiftLeft(shiftLength << 1)
+                .add(productBD)
+                .add(productABCD.subtract(productAC.add(productBD))
+                        .shiftLeft(shiftLength));
+    }
 
     public long karaMultiply(long x, long y) {
         if (x == 0 || y == 0) {
@@ -21,24 +50,24 @@ public class KaratsubaMultiplier {
         int lengthX = getBitLength(x);
         int lengthY = getBitLength(y);
 
-        int divLength = Math.max(lengthX, lengthY) / 2;
+        int shiftLength = Math.max(lengthX, lengthY) / 2;
 
-        if (divLength == 0) {
-            return x * y;
+        if (shiftLength == 0) {
+            return x & y;
         }
 
-        long a = x >> divLength;
-        long b = x & (~(a << divLength));
-        long c = y >> divLength;
-        long d = y & (~(c << divLength));
+        long a = x >> shiftLength;
+        long b = x & (~(a << shiftLength));
+        long c = y >> shiftLength;
+        long d = y & (~(c << shiftLength));
 
         long productAC = karaMultiply(a, c);
         long productBD = karaMultiply(b, d);
         long productABCD = karaMultiply(a + b, c + d);
 
-        long result = (productAC << (divLength * 2))
+        long result = (productAC << (shiftLength << 1))
                 + productBD +
-                ((productABCD - (productAC + productBD)) << divLength);
+                ((productABCD - (productAC + productBD)) << shiftLength);
 
         if (negative) {
             result = -result;
